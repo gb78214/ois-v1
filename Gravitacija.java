7733dd8fcf6bbbffb82cc2801e4fe4d4f5cbb16a
Gaj Brus <160838104+gb78214@users.noreply.github.com>
2024-03-03 17:07:18 +0100

    Add files via upload

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,8 +1,15 @@
 public class Gravitacija {
     public static void main(String[] args) {
         System.out.println("OIS je zakon!");
-    }
 
+        izpis(0.0);
+        izpis(10.0);
+        izpis(1000.0);
+        izpis(100000.0);
+    }
+    public static void izpis(double visina){
+		System.out.printf("Gravitacijski pospešek na %.2f km nadmorske višine je %.2f m/s^2\n", visina, izracunPospeska(visina*1000));
+	}
     public static double izracunPospeska(double visina){
         return 6.674 * 5.972 * Math.pow(10, 13) / Math.pow(6.371 * 1000000 + visina, 2);
     }
