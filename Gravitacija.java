7665abb7fa58820661e117dcf47475b590e72687
Miha-2 <miha.perpar2@gmail.com>
2024-03-03 14:10:51 +0100

    Dodaj izračun pospeška #4

--- Gravitacija.java
+++ Gravitacija.java
@@ -2,4 +2,8 @@ public class Gravitacija {
     public static void main(String[] args) {
         System.out.println("OIS je zakon!");
     }
+
+    public static double izracunPospeska(double visina){
+        return 6.674 * 5.972 * Math.pow(10, 13) / Math.pow(6.371 * 1000000 + visina, 2);
+    }
 }
